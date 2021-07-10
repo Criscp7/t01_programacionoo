69457a0aa3aed99d49702d1836f0eb4562a6d664
Cristian Pozo <Cristian Pozo@DESKTOP-CP0BFFG>
2021-07-09 20:18:03 -0500

    7

---
+++ src/Debern1/Ejercicioenclase.java
@@ -0,0 +1,20 @@
+package Debern1;
+import java.util.Scanner;
+
+
+public class Tablas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Introduce un numero entero:");
+		n=sc.nextInt();
+		System.out.println("Tabla del"+n);
+		for(int i=1;i<=12;i++) {
+			Ssytem.out.println(n+"*"+i+"="n*i);
+		}
+		
+
+	}
+
+}
